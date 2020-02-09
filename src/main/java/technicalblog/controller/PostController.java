dfda3ab61b63108c7a7f9f601b73f30db137ec3a
Manish hooda <mhooda2412@gmail.com>
2020-02-09 16:42:51 +0530

    second milestone

--- src/main/java/technicalblog/controller/PostController.java
+++ src/main/java/technicalblog/controller/PostController.java
@@ -8,19 +8,15 @@ import technicalblog.model.Post;
 import technicalblog.service.PostService;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 @Controller
-public class HomeController {
+public class PostController {
     @Autowired
     private PostService postService;
-    @RequestMapping("/")
-    public String getAllPosts(Model model) {
-
-        ArrayList<Post> posts = postService.getAllPosts();
-        model.addAttribute("posts", posts);
-
-        return "index";
-
+    @RequestMapping("posts")
+    public String getUsersPosts(Model model){
+        ArrayList<Post> posts = postService.getOnePost();
+        model.addAttribute("posts",posts);
+        return "posts";
     }
 }
