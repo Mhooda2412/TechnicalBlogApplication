7c8763afb03bd372c1159b05be5878d5ed7841d8
Manish hooda <mhooda2412@gmail.com>
2020-03-07 15:05:42 +0530

    all database maped

--- src/main/java/technicalblog/controller/PostController.java
+++ src/main/java/technicalblog/controller/PostController.java
@@ -5,10 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import technicalblog.model.Category;
 import technicalblog.model.Post;
+import technicalblog.model.User;
 import technicalblog.service.PostService;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
@@ -16,7 +21,8 @@ public class PostController {
     private PostService postService;
     @RequestMapping("posts")
     public String getUsersPosts(Model model){
-        ArrayList<Post> posts = postService.getOnePost();
+
+        List<Post> posts = postService.getAllPosts();
         model.addAttribute("posts",posts);
         return "posts";
     }
@@ -26,8 +32,46 @@ public class PostController {
     }
 
     @RequestMapping(value = "/posts/create", method = RequestMethod.POST)
-    public String createPost(Post newPost) {
+    public String createPost(Post newPost, HttpSession session) {
+        User user = (User) session.getAttribute("loggeduser");
+        newPost.setUser(user);
+
+        if (newPost.getSpringBlog() != null) {
+            Category springBlogCategory = new Category();
+            springBlogCategory.setCategory(newPost.getSpringBlog());
+            newPost.getCategories().add(springBlogCategory);
+        }
+
+        if (newPost.getJavaBlog() != null) {
+            Category javaBlogCategory = new Category();
+            javaBlogCategory.setCategory(newPost.getJavaBlog());
+            newPost.getCategories().add(javaBlogCategory);
+        }
+
         postService.createPost(newPost);
+        return "redirect:/posts";
+    }
+
+    @RequestMapping(value = "/editPost", method = RequestMethod.GET)
+    public String editPost(@RequestParam(name="postId") Integer postId, Model model) {
+        Post post = postService.getPost(postId);
+        model.addAttribute("post",post);
+        return "posts/edit";
+    }
+
+
+    @RequestMapping(value = "/editPost", method = RequestMethod.PUT)
+    public String editPostSubmit(@RequestParam(name="postId") Integer postId, Post updatedPost, HttpSession session) {
+
+        updatedPost.setId(postId);
+        User user = (User)session.getAttribute("loggeduser");
+        updatedPost.setUser(user);
+        postService.updatePost(updatedPost);
+        return "redirect:/posts";
+    }
+    @RequestMapping(value = "/deletePost", method = RequestMethod.DELETE)
+    public String deletePostSubmit(@RequestParam(name="postId") Integer postId) {
+        postService.deletePost(postId);
         return "redirect:/posts";
     }
 }
